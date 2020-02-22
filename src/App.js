2aee66dc1f414766239edea1816063d096b71738
Kyle Price <kylep5587@gmail.com>
2020-02-22 15:06:50 -0500

    Test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component { // the render method is built into Component
+  constructor() {
+    super(); // Calls React.Component's constructor
+
+    this.state = {
+      monsters: []
+    };
+  }
+
+  // Called when a component is rendered on the page for the first time
+  componentDidMount() { // Populates monsters array with data from the url
+    fetch('https://jsonplaceholder.typicode.com/users')   // Makes call to the url
+    .then(response => response.json())                    // Gets JSON object from url
+    .then(users => this.setState({ monsters: users }));   // Gets users portion of the JSON object and populates the monsters array
+  }
+
+  // When state changes, render() is called again
+  render() {
+    return (
+      <div className="App">
+        {
+          // map causes the return of the method to iterate through each element in the array
+          //.monsters.map() will iterate through each element in the monsters array
+          // key tells react which element in the monster array to update
+          this.state.monsters.map(monster => (
+            <h1 key={monster.id}> { monster.name }</h1>
+          ))
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
