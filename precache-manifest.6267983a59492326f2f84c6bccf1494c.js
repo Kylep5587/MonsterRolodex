3458e0ca62248599e9332b485a8ce38f15596c21
Kyle Price <kylep5587@gmail.com>
2020-02-26 20:23:54 -0500

    Updates

---
+++ precache-manifest.6267983a59492326f2f84c6bccf1494c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ce32b19e05b70f46bebd9db14803f0",
+    "url": "/MonsterRolodex/index.html"
+  },
+  {
+    "revision": "05eac73c8f106915f4a3",
+    "url": "/MonsterRolodex/static/css/main.15ded02e.chunk.css"
+  },
+  {
+    "revision": "f639c8c13fbe7051c160",
+    "url": "/MonsterRolodex/static/js/2.5724b15e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/MonsterRolodex/static/js/2.5724b15e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "05eac73c8f106915f4a3",
+    "url": "/MonsterRolodex/static/js/main.2ab88460.chunk.js"
+  },
+  {
+    "revision": "f6543e4fd06c720b0323",
+    "url": "/MonsterRolodex/static/js/runtime-main.b86ffaf5.js"
+  }
+]);
